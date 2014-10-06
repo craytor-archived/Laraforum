57acad5e8ca8995c468d9fd25cdf7f1ddeb45392
David <dponce116@gmail.com>
2014-10-06 14:22:48 -0400

    Controllers no longer extend Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,21 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller;
-
-class HomeController extends Controller {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	$router->get('/', 'HomeController@index');
-	|
-	*/
+class HomeController {
 
 	/**
 	 * @Get("/", as="home")
